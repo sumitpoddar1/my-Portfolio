d8dce3130442c51ba86eaa482c73cddbef8e906c
Sumit Poddar <sumitpoddar00001@gmail.com>
2024-12-26 17:48:17 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,37 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>Welcome to My Portfolio</h1>
+      <p>As a passionate Software Engineer with expertise in full-stack development, I specialize in building scalable, user-friendly applications. With a BTech in Software Engineering, I have hands-on experience in Python, JavaScript, React, and Node.js. I focus on delivering efficient code, solving complex problems, and optimizing user experience. Constantly learning new technologies, I aim to contribute to innovative teams by developing high-quality software solutions that drive performance and exceed client expectations.</p>
+      <br />
+      <h2><b>Technical Skills</b></h2>
+    <ul>
+        <li><strong>Programming Languages:</strong> Python, JavaScript, Java, C++</li>
+        <li><strong>Web Development:</strong> HTML, CSS, React.js, Node.js</li>
+        <li><strong>Database Management:</strong> SQL, MongoDB, PostgreSQL</li>
+        <li><strong>Version Control:</strong> Git, GitHub</li>
+        <li><strong>Software Development:</strong> Agile methodologies, Test-driven development (TDD)</li>
+        <li><strong>Tools & Platforms:</strong> Docker, Kubernetes, Jenkins, AWS, Azure</li>
+        <li><strong>Debugging & Testing:</strong> Unit testing, integration testing, debugging tools</li>
+    </ul>
+
+    <br />
+    <h2><b>Soft Skills</b></h2>
+    <ul>
+        <li><strong>Problem-Solving:</strong> Analytical thinking and logical reasoning</li>
+        <li><strong>Collaboration:</strong> Teamwork in cross-functional environments</li>
+        <li><strong>Communication:</strong> Explaining technical concepts to non-technical stakeholders</li>
+        <li><strong>Time Management:</strong> Prioritizing tasks and meeting deadlines</li>
+        <li><strong>Adaptability:</strong> Learning new technologies quickly</li>
+    </ul>
+      <div>
+        <br />
+        <h3><b>Projects</b></h3>
+        <ul>
+          <li><a href="https://github.com/my-project">Project 1</a></li>
+          <li><a href="https://github.com/my-project">Project 2</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
